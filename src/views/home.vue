8303c21b33e888583c66945427bb12c658a2f3dc
unknown <>
2017-05-18 15:36:00 +0800

    update

---
+++ src/views/home.vue
@@ -0,0 +1,30 @@
+//home.vue
+<template>
+    <div>
+        <ol>
+            <li v-for="todo in todos">
+                {{todo.text}}
+            </li>
+        </ol>
+        <button @click="eClick">click</button>
+    </div>
+</template>
+<script>
+    export default {
+        name:'indexP',
+        data(){
+            return {
+                todos:[
+                    { text: 'Learn JavaScript' },
+                    { text: 'Learn Vue' },
+                    { text: 'Build something awesome' }
+                ]
+            }
+        },
+        methods:{
+            eClick(){
+                console.log(999);
+            }
+        }
+    }
+</script>
